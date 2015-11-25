2494ad9186c8e4bda3f6f96581d544ebb426f036
Sam Johnson <sam@80pct.com>
2015-11-24 22:52:03 -0500

    Get index page loading with basic page

--- blog.go
+++ blog.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"github.com/GeertJohan/go.rice"
+	"html/template"
+	"net/http"
 )
 
+const STATIC_BOX string = "static"
+
+func LoadTemplate(name string, box *rice.Box) (*template.Template, error) {
+	tName := fmt.Sprintf("%s.html", name)
+	tString, err := box.String(tName)
+	if err != nil {
+		return nil, err
+	}
+	return template.New(name).Parse(tString)
+}
+
+type Data struct {
+	Word string
+}
+
+func indexHandler(box *rice.Box) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		t, _ := LoadTemplate("index", box)
+		t.Execute(w, &Data{Word: "helloworld"})
+	}
+}
+
 func main() {
-	fmt.Print("HELLLOOOOO")
+	// Web pages
+	box := rice.MustFindBox("static")
+	http.HandleFunc("/", indexHandler(box))
+
+	// Static files
+	staticFiles := http.StripPrefix("/static/", http.FileServer(box.HTTPBox()))
+	http.Handle("/static/", staticFiles)
+
+	http.ListenAndServe(":8080", nil)
 }
