2d3d827c1052e8725a68304196e1817b83ea1f7e
Sam Johnson <sam@80pct.com>
2015-12-05 22:51:16 -0500

    Render blog files as HTML

--- blog.go
+++ blog.go
@@ -4,11 +4,37 @@ import (
 	"fmt"
 	"github.com/GeertJohan/go.rice"
 	"html/template"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 const STATIC_BOX string = "static"
 
+func indexHandler(box *rice.Box) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		posts, _ := posts()
+		log.Printf("Posts: %v", posts)
+		t, _ := LoadTemplate("index", box)
+		t.Execute(w, posts)
+	}
+}
+
+// Read all the HTML post fragments and return them as Posts
+func posts() (*[]template.HTML, error) {
+	dir := "./fragments"
+	files, _ := ioutil.ReadDir(dir)
+
+	posts := make([]template.HTML, len(files))
+	for i, file := range files {
+		data, _ := ioutil.ReadFile(dir + "/" + file.Name())
+		p := template.HTML(data)
+		posts[i] = p
+	}
+	return &posts, nil
+}
+
+// Load an html template from a rice box
 func LoadTemplate(name string, box *rice.Box) (*template.Template, error) {
 	tName := fmt.Sprintf("%s.html", name)
 	tString, err := box.String(tName)
@@ -16,17 +42,6 @@ func LoadTemplate(name string, box *rice.Box) (*template.Template, error) {
 		return nil, err
 	}
 	return template.New(name).Parse(tString)
-}
-
-type Data struct {
-	Word string
-}
-
-func indexHandler(box *rice.Box) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		t, _ := LoadTemplate("index", box)
-		t.Execute(w, &Data{Word: "helloworld"})
-	}
 }
 
 func main() {
