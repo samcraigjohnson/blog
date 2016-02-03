e7a7498700635a7f302faa5faa9c60324e21fadc
Sam Johnson <sjohnson540@gmail.com>
2016-02-03 14:47:17 -0500

    Add shortening for home page

--- post.go
+++ post.go
@@ -4,24 +4,34 @@ import (
 	"html/template"
 	"fmt"
 	"io/ioutil"
-//	"regexp"
+	"log"
+	"regexp"
 )
 
 type Post struct {
 	Content string
 	FirstP string
 	Link string
+	Title string
+	Date string
 }
 
 func NewPost(fileLocation string) *Post {
 	//	r := regexp.Compile("<p>[.]*</p>")
 	data, _ := ioutil.ReadFile(fileLocation)
 	link := fmt.Sprintf("<a href=\"%s\">%s</a>", fileLocation, "read more")
-	return &Post{Content: string(data), Link: link}
+	html := string(data)
+	return &Post{
+		Content: html,
+		Link: link,
+		Title: FindTag("h1", html),
+		Date: FindTag("h3", html),
+		FirstP: FindTag("p", html),
+	}
 }
 
 func (p *Post) ToIndexHTML() template.HTML {
-	html := p.Content + p.Link
+	html := p.Title + p.Date + p.FirstP + p.Link
 	return template.HTML(html)
 }
 
@@ -29,4 +39,12 @@ func (p *Post) ToIndexHTML() template.HTML {
 // the first paragraph
 func (p *Post) shorten() {
 	
+}
+
+
+func FindTag(tag string, html string) string {
+	regex := fmt.Sprintf("<%s( class=.*)?( id=.*)?>.*</%s>", tag, tag)
+	log.Printf("Regex: %v", regex)
+	r, _ := regexp.Compile(regex)
+	return r.FindString(html)
 }
